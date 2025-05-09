5a551833556d70f3ac2935a177f1cdd2f4ebf510
narharikale <n7kale@gmail.com>
2025-05-09 21:08:35 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>another diw</p>
+      <h1>this oss sdflsfsf fflsksf fflkskdfs fs fjsdfsf </h1>
+    </>
+  );
+}
+
+export default App;
